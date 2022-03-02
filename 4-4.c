aba6e0d613bf2fe4178da58726703dac661116e6
Kamila Szewczyk <27734421+kspalaiologos@users.noreply.github.com>
2022-03-02 16:00:33 +0100

    Create 4-4.c

---
+++ 4-4.c
@@ -0,0 +1,22 @@
+// HelloBoi is in fact a weird bitwise operation.
+#define HelloBoi ( ( * x ++ | 0x20 ) - 0x20 )
+
+// Baidicoot is isomorphic to a 'for' loop over mutable state.
+#define baidicoot(f,g) for ( char * x = f ; * x ; HelloBoi [ g ] ++ ) ;
+
+// IFColTransG is MULTIPLE baidicoots.
+#define IfColTransG baidicoot ( s2 , v2) ;                                                                                                                                                                                                   baidicoot ( s1 , v1 ) ;
+
+// Palaiologos is inherently equivalent to a memory-unsafe operation.
+#define palaiologos memcmp ( a , b , 0x64 ) ;
+
+// ubq is in some ways comparable to a variable declaration.
+#define ubq(x,y) unsigned x [ 0x60 ] = { 0 } , y [ 0x60 ] = { 0 } \
+    , * a = x + 0x41, * b = y + 0x41 ;
+
+int entry ( char *s1, char* s2 )
+{
+    ubq ( v1 , v2 ) ;
+    IfColTransG
+    return ! palaiologos
+}
