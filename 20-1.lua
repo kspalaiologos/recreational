cda40ddb5f2f6500fa8564d7d4779fb11e7ff7f1
Kamila Szewczyk <27734421+kspalaiologos@users.noreply.github.com>
2022-07-24 20:58:08 +0200

    Create 20-1.lua

---
+++ 20-1.lua
@@ -0,0 +1,23 @@
+                                             load(([[f
+                                            unction@I(s
+                         ,p,v)re           turn@s:sub(#"
+                   -",p-#"$")..v..s:su     b(p+#"#")end;
+                function@M(b,p)local@v,c=   -#"#",-#"$_
+             ";for@i=#"#",#'_$(.)(.)$'do@if@ b:sub(i,i
+           )=="."then@b=I(b,i,p);l      ocal@
+          a=-m(b,p=="x"and"o"or"          x")if
+         @a>c@then@v,c=i,a@end@b           =I(b,
+        i,".")end@end@return@v,c          @end;fu
+        nction@m(S,p)local@w,A,B,C       ='.',{1,
+       4,7,1,2,3,1,3},{2,5,8,4,5,6,5,5},{3,6,9,7,8
+       ,9,9,7};for@i=#"#",#'#&*!__[]'@do@local@a,b
+       ,c=S:sub(A[i],A[i]),S:sub(B[i],B[i]),S:sub(
+       C[i],C[i]);if@a==b@and@a==c@and@a~="."then@
+        w=a@end@end@if@w~='.'then@return@w==p@and
+        #"."or-#"."end@return({M(S,p)})[#"[]"]end
+         ;function@entry(s)local@b=s:gsub("%\n",
+          '');return@I(b,M(b,'x'),"x")end]]):--
+            gsub("[ \n]",""):gsub("@"," "))()
+              --[=[[_](#_-_[_._]/_[_._]).._
+                [_[#_]_)]+#_/_[_._],_[_[_
+                    [#_]](#_)]+#_-]=]
